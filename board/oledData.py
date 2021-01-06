903add71bda2f588f1575b5a19ed300f835f294c
Daniel Nduati <dannduati2@gmail.com>
2021-01-06 15:27:35 +0300

    Updated the ui display function

--- board/oledData.py
+++ board/oledData.py
@@ -11,8 +11,7 @@ display = ssd1306.SSD1306_I2C(128,64,i2c)
 wifi.connect()
 server.createSocket()
 
-
-def displayVals(x,y,s):
+def drawUI():
     #clear the display
     display.fill(0)
     display.rect(0,0,128,64,1)
@@ -22,14 +21,22 @@ def displayVals(x,y,s):
     display.text('x',20,5)
     display.text('y',20,26)
     display.text('start',5,47)
+
+def displayVals(x,y,s):
     display.text('{}'.format(str(x)),48,5)
     display.text('{}'.format(str(y)),48,26)
     display.text('{}'.format(str(s)),48,47)
     display.show()
 
-while True:
-    values = server.getData()
+def main():
+    drawUI()
+    try:
+        values = server.getData()
+    except:
+        print("Error trying to get data check network")
     x = values[0]
     y = values[1]
     s = values[2]
     displayVals(x,y,s)
+while True:
+    main()
